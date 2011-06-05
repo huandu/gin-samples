bb693f0b3ff9e7adf920f9d40d60118ba4c8d51a
liruqi <liruqi@gmail.com>
2011-06-06 01:35:36 +0800

    use time to find position, instead of frame number

--- o2/bms/bms_dump.py
+++ o2/bms/bms_dump.py
@@ -9,6 +9,8 @@ def usage():
     print """Usage: 
     -d:
         debug
+    -f:
+        output beat
     -i, --input:
         input file path
     -o, --output:
@@ -22,9 +24,9 @@ def main(argv):
 
     _debug = False
     _input = _output = ''
-    _fps = 0
+    _frame = False
     try:
-        opts, args = getopt.getopt(argv, 'hi:o:f:d', ['help', 'input=', 'output='])
+        opts, args = getopt.getopt(argv, 'hi:o:fd', ['help', 'input=', 'output='])
     except getopt.GetoptError:
         usage()
         sys.exit(2)
@@ -40,7 +42,7 @@ def main(argv):
         elif opt in ('-o', '--output'):
             _output = arg
         elif opt in ('-f', '--fps'):
-            _fps = int(arg)
+            _frame = True 
 
 
     bms = BMS().parse(_input)
@@ -52,10 +54,12 @@ def main(argv):
         'body': bms.body,
         'length': len(bms.body)
     }
-    if _fps > 0:
+    if _frame > 0:
         frames = {}
         for noteId in body:
+            
             note = body[noteId]
+            frames[ int(noteId) ] = {}
             divide = {}
             for pos in note:
                 if (len(note[pos]) > len(divide)):
@@ -63,7 +67,6 @@ def main(argv):
             for offset in divide:
                 if _debug:
                     print (noteId, bms.header["DIVIDE"], offset, bms.header["BPM"])
-                frameId = int( 60.0*(int(noteId) * bms.header["DIVIDE"] + offset)*_fps / float(bms.header["BPM"])/ bms.header["DIVIDE"] )
                 theFrame = {}
                 for pos in note:
                     if offset in note[pos]:
@@ -71,12 +74,12 @@ def main(argv):
                         if val:
                             theFrame[pos] = val
                 if (len(theFrame)):
-                    frames[frameId] = theFrame
+                    frames[int(noteId)][offset] = theFrame
 
         output["frames"] = frames
     
     if _debug:
-        print _fps
+        print output["frames"] 
         print bms.header 
 
     o = open(_output, 'w')
